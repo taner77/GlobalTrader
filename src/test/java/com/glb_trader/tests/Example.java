e72581c662ef2bef1bbf3887e8ebccd9dad1ce72
taner77 <tcelikim@gmail.com>
2020-09-21 23:47:22 -0400

    local taner den

--- src/test/java/com/glb_trader/tests/Example.java
+++ src/test/java/com/glb_trader/tests/Example.java
@@ -1,4 +1,9 @@
 package com.glb_trader.tests;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class Example {
+    public static void main(String[] args) {
+        System.out.println("from taner local");
+    }
 }
