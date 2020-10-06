e41d14e59d28547b0d0357f6c8afe7d4e1ce4415
fazlikuzhan <fzlkzhn46@gmail.com>
2020-10-05 21:45:53 -0700

    gr_6 done

---
+++ src/test/java/com/glb_trader/utilities/ConfigurationReader.java
@@ -0,0 +1,43 @@
+package com.glb_trader.utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    //Bu class in amaci, configuration.properties dosyasindaki
+    //verileri (test data ) okumaktir
+
+    //create Properties intance
+    private  static Properties properties;
+
+
+
+    //Properties i bu dosyaya yukluyoruz
+    //Daha sonra properties dosyasini okuyacagiz
+
+    static {
+        //  String path = "C:\\Users\\steel\\IdeaProjects\\mytestNGframework\\configuration.properties";
+        String path = "configuration.properties";
+        try {
+            FileInputStream file = new FileInputStream(path); //FileInputStream kullanarak bir dosya aciyoruz
+            properties = new Properties();
+            properties.load(file);
+        } catch (Exception e) {
+            // System.out.println("File path bulunamadi");
+            e.printStackTrace();
+        }
+    }
+    //Okumak icin static bir metod olusturalim.
+    //Bu metod kullanici anahtar kelimeyi girdiginde(key) , value return eder.
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+
+//public static void main(String[] args) {
+//    System.out.println(properties.getProperty("browser"));
+//}
+}
+
+
